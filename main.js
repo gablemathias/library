132b419fe208c22569e7e4ebfa94328554d14d48
Gabriel Mathias <gablemathias@icloud.com>
2024-06-20 15:01:25 -0300

    add main files and connect with server

---
+++ main.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(author, title, pagesNumber, isRead = false) {
+  this.author = author;
+  this.title = title;
+  this.pagesNumber = pagesNumber;
+  this.isRead = isRead;
+}
+
+// function addBookToLibrary(book) {
+  
+// }
