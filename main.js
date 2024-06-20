44e21fecd20ec4a16931fc7613d27ffc745bcdcb
Gabriel Mathias <gablemathias@icloud.com>
2024-06-20 16:44:26 -0300

    book being added after onclick event

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const myLibrary = [];
 
-function Book(author, title, pagesNumber, isRead = false) {
+function Book(author, title, pagesNumber, isRead) {
   this.author = author;
   this.title = title;
   this.pagesNumber = pagesNumber;
   this.isRead = isRead;
 }
 
-// function addBookToLibrary(book) {
-  
-// }
+function addBookToLibrary(author,title,pagesNumber,isRead) {
+  myLibrary.push(author,title,pagesNumber,isRead);
+}
+const submit = document.querySelector("#submit");
+
+submit.onclick = () => {
+  const author = document.querySelector("#author").value
+  const title = document.querySelector("#title").value
+  const pagesNumber = document.querySelector("#pages-number").value
+  const isRead = document.querySelector("#is-read").value
+  alert(`Hello ${author}, nice to see you!`);
+
+  addBookToLibrary(author,title,pagesNumber,isRead);
+
+  alert(myLibrary);
+};
